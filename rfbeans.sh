92628dacac828e2549c6717a23f98974001eac21
rpawlik <ramsey.pawlik@rackspace.com>
2013-02-05 14:04:43 -0600

    first commit

---
+++ rfbeans.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+ 
+# REFRIED BEANS
+# 'soft' rekick a rcps environment
+ 
+#NOTE: need this to run on compute nodes as well
+
+function do_dsh {
+
+dsh -m localhost -g compute $*
+
+}
+ 
+echo "PURGING THE BEANS"
+ 
+apt-get -y purge `dpkg -l | awk '/mysql/ {print $2}'` >/dev/null
+apt-get -y purge `dpkg -l | awk '/keystone/ {print $2}'` >/dev/null
+apt-get -y purge `dpkg -l | awk '/glance/ {print $2}'` >/dev/null
+ 
+rm -rf /root/.my.cnf
+rm -rf /etc/mysql/grants.sql
+rm -rf /var/lib/mysql
+rm -rf /var/chef
+rm -f /var/cache/local/preseeding/mysql-server.seed
+rm -rf /etc/glance /var/lib/glance
+rm -rf /etc/keystone
+ 
+do_dsh apt-get -y purge `dpkg -l | awk '/nova/ {print $2}'` >/dev/null
+do_dsh rm -rfv /etc/nova /var/lib/nova/ /var/log/nova/ >/dev/null
+do_dsh apt-get -y autoremove >/dev/null
+ 
+do_dsh dpkg -P -a >/dev/null
+echo purge | debconf-communicate mysql-server-5.0
+echo purge | debconf-communicate mysql-server-5.5
+ 
+ 
+echo "REFRYING THE BEANS"
+ 
+if [[ ! -d /opt/rpcs/chef-cookbooks ]] ; then
+    git clone --depth 1 --recursive http://github.com/rcbops/chef-cookbooks.git /opt/rpcs/chef-cookbooks
+fi
+ 
+knife cookbook upload -ao /opt/rpcs/chef-cookbooks/cookbooks >/dev/null
+knife role from file /opt/rpcs/chef-cookbooks/roles/*.rb >/dev/null
+
+#NOTE: need to remove nodes/readd to chef
+
+knife node list | egrep 'infra|novacpu|controller|compute' > /tmp/node_list.out 
+
+for i in $(cat /tmp/node_list.out); do knife node delete -y $i; knife client delete -y $i; done >/dev/null
+
+do_dsh rm -rf /etc/chef/client.pem
+do_dsh chef-client >/dev/null
+knife cookbook delete apt 1.8.4 -y >/dev/null
+for i in $(egrep 'infra|controller' /tmp/node_list.out); do knife node run_list add $i 'role[single-controller]'; done >/dev/null
+for i in $(egrep 'compute|novacpu' /tmp/node_list.out); do knife node run_list add $i 'role[single-compute]'; done >/dev/null
+
+chef-client >/dev/null
+do_dsh chef-client >/dev/null
+nova keypair-add --pub-key /root/.ssh/id_rsa.pub controller-id_rsa >/dev/null
+mysql nova  -e 'UPDATE fixed_ips SET reserved=1 LIMIT 10' >/dev/null
+# might need to run `mysqladmin flush-hosts` on infra if computes are making too many connections
